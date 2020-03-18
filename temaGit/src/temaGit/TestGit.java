b24d482c63a11cde097c7a2c5039d37a3ccba3f7
Intel <Intel@DESKTOP-Tinna>
2020-03-18 15:07:03 +0200

    clasa TestGit

---
+++ temaGit/src/temaGit/TestGit.java
@@ -0,0 +1,11 @@
+package temaGit;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git ! Denumirea proiectului de licenta "
+				+ "este Aplicatie mobila in domeniul scenaristicii.");
+		
+	}
+
+}
