2e13f9f03425bea93f85ed070a6dd65572b62dc1
Intel <Intel@DESKTOP-Tinna>
2020-03-18 15:25:47 +0200

    Clasa Scena a fost creata, avand 3 atribute si un constructor

---
+++ temaGit/src/temaGit/Scena.java
@@ -0,0 +1,16 @@
+package temaGit;
+
+public class Scena {
+	private String denumireScena;
+	private int numarPersonaje;
+	private String descriere;
+	
+	public Scena(String denumireScena, int numarPersonaje, String descriere) {
+		super();
+		this.denumireScena = denumireScena;
+		this.numarPersonaje = numarPersonaje;
+		this.descriere = descriere;
+	}
+	
+	
+}
